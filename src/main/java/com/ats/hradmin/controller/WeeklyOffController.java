8bc265b15eb172395cc2164450edc0c0d8a9b4d8
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2019-04-24 12:21:41 +0530

    //neha

---
+++ src/main/java/com/ats/hradmin/controller/WeeklyOffController.java
@@ -0,0 +1,19 @@
+package com.ats.hradmin.controller;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Scope("session")
+public class WeeklyOffController {
+
+	DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	Date now = new Date();
+	String curDate = dateFormat.format(new Date());
+	String dateTime = dateFormat.format(now);
+
+}
