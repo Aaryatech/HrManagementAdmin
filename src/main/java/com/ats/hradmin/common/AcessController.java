a47447e53aaa710d449ea9b93854b390a797795f
lenovo <lenovo@lenovo-Lenovo-ideapad-330-15IKB>
2019-05-03 12:02:10 +0530

    //harsha

---
+++ src/main/java/com/ats/hradmin/common/AcessController.java
@@ -0,0 +1,102 @@
+package com.ats.hradmin.common;
+
+import java.util.List;
+
+import com.ats.hradmin.model.AccessRightModule;
+import com.ats.hradmin.model.Info;
+
+
+public class AcessController {
+	
+
+	public static Info checkAccess(String currentMapping, String viewMapping, String view, int add, int edit,
+			int delete, List<AccessRightModule> newModuleList) {
+
+		Info info = new Info();
+
+		try {
+
+			//System.out.println("in function newModuleList " + newModuleList);
+
+			int viewMappingFound = 0;
+
+			for (int i = 0; i < newModuleList.size(); i++) {
+
+				for (int j = 0; j < newModuleList.get(i).getAccessRightSubModuleList().size(); j++) {
+
+					if (newModuleList.get(i).getAccessRightSubModuleList().get(j).getSubModuleMapping().equals(viewMapping)) {
+						viewMappingFound = 1;
+
+						//System.out.println("Mapping Found");
+
+						if (view.equals("1")) {
+
+							//System.out.println("in view");
+							info.setError(false);
+							info.setMsg("Have Access");
+							
+						} else if (add == 1) {
+							
+							//System.out.println("in add");
+							
+							if (newModuleList.get(i).getAccessRightSubModuleList().get(j).getAddApproveConfig()== add) {
+								//System.out.println("in add if");
+								info.setError(false);
+								info.setMsg("Have Access");
+							} else {
+								//System.out.println("in add else");
+								info.setError(true);
+								info.setMsg("Access denied");
+							}
+						} else if (edit == 1) {
+
+							//System.out.println("in edit");
+							if (newModuleList.get(i).getAccessRightSubModuleList().get(j).getEditReject() == edit) {
+								//System.out.println("in edit if");
+								info.setError(false);
+								info.setMsg("Have Access");
+							} else {
+								//System.out.println("in edit else");
+								info.setError(true);
+								info.setMsg("Access denied");
+							}
+						} else if (delete == 1) {
+							//System.out.println("in delete");
+							if (newModuleList.get(i).getAccessRightSubModuleList().get(j).getDeleteRejectApprove()
+									== (delete)) {
+								//System.out.println("in edit if");
+								info.setError(false);
+								info.setMsg("Have Access");
+							} else {
+								//System.out.println("in edit else");
+								info.setError(true);
+								info.setMsg("Access denied");
+							}
+						} else {
+							//System.out.println("in else");
+							info.setError(true);
+							info.setMsg("Access denied");
+						}
+
+					}
+				}
+
+			}
+
+			if (viewMappingFound == 0) {
+				//System.out.println("mapping not found");
+				info.setError(true);
+				info.setMsg("Access Denied");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			info.setError(true);
+			info.setMsg("access denied ");
+		}
+
+		return info;
+
+	}
+
+}
