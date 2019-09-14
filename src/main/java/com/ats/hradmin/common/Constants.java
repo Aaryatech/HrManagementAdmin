69bd43f6f1235d9b14715588e79ba42a6bff7cc2
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2019-09-14 17:27:07 +0530

    /Akshay

--- src/main/java/com/ats/hradmin/common/Constants.java
+++ src/main/java/com/ats/hradmin/common/Constants.java
@@ -6,13 +6,13 @@ import org.springframework.web.client.RestTemplate;
 public class Constants {
  
 	public static final String url="http://localhost:8093/";
-	//public static final String url="http://10.0.0.4:8181/HrManagementSystem/";
+	//public static final String url="http://ifbthrms.infrabeat.com:8181/HrManagementSystem/";
 	//public static final String url="http://132.148.143.124:8080/HrManagementSystem/";
 	public static RestTemplate rest = new RestTemplate(); 
 	//public static final String imageSaveUrl = "/home/lenovo/Downloads/";
 	public static final String imageSaveUrl = "/opt/tomcat/webapps/hr/";
-	//public static final String getImageSaveUrl = "http://192.168.2.14:8080/media/hr/"; 
-	public static final String getImageSaveUrl = "http://10.0.0.4:8181/hr/";
+	public static final String getImageSaveUrl = "http://ifbthrms.infrabeat.com:8181/hr/"; 
+	//public static final String getImageSaveUrl = "http://10.0.0.4:8181/hr/";
 	public static String[] values = { "jpg", "jpeg", "gif", "png" };
 	public static String[] allextension = { "txt", "doc", "pdf", "xls", "jpg", "jpeg", "gif", "png" };
 	 public static RestTemplate getRestTemplate() {
